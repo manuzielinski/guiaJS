6ca63f664814ca368736dbaef02dcb56da956a38
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-10-04 22:03:37 -0300

    upload

--- guiaZielinski.js
+++ guiaZielinski.js
@@ -116,6 +116,106 @@ if(promedio2 >= 4) {
     document.write("el alumno ha reprobado");
 }
 
+// ejercicio 3
+
+let numero;
+
+numero = prompt("ingrese el numero");
+
+if (numero > 1 && numero < 99) {
+    if (numero >= 10 ) {
+        document.write("el numero contiene dos digitos");
+    }else {
+    document.write("el numero no es valido");
+    }
+}
+
+// ejercicio 1 navidad
+
+let dia;
+let mes;
+let ano;
+
+dia = prompt("ingrese el dia");
+mes = prompt("ingrese el mes");
+ano = prompt("ingrese el ano");
+
+if (dia == 25 && mes == 12) {
+    document.write('el dia corresponde a navidad');
+}else {
+    document.write('el dia no corresponde a navidad');
+}
+
+// ejercicio 2 valores
+
+let valor11;
+let valor22;
+let valor33;
+
+valor11 = Number(prompt("ingrese el primer valor"));
+valor22 = Number(prompt("ingrese el segundo valor"));
+valor33 = Number(prompt("ingrese el tercer valor"));
+
+if (valor11 == valor22 && valor22 == valor33) {
+    suma = valor11 + valor22;
+    multiplicacion = suma * valor33;
+    document.write(`el valor de la suma es: ${suma}`);
+    document.write(`<br>el valor de la multiplicacion es: ${multiplicacion}`);
+}else {
+    document.write("los valores no son iguales");
+}
+
+// ejercicio 3
+
+let valor112;
+let valor222;
+let valor332;
+
+valor112 = Number(prompt("ingrese el primer valor"));
+valor222 = Number(prompt("ingrese el segundo valor"));
+valor332 = Number(prompt("ingrese el tercer valor"));
+
+if(valor112 <= 10 || valor222 <= 10 || valor332 <= 10) {
+    document.write("Los valores o alguno de estos es menor a 10");
+}else {
+    document.write("Los valores son mayores o iguales a 10"); 
+}
+
+// ejercicio 4
+
+let x= prompt("ingrese el primer valor");
+let y = prompt("ingrese el segundo valor");
+
+
+if (x < 0 && y > 0) {
+    document.write('se encuentra en el primer cuadrante');
+}else if (x > 0 && y > 0) {
+    document.write('se encuentra en el segundo cuadrante');
+}else if (x < 0 && y < 0) {
+    document.write('se encuentra en el tercer cuadrante');
+}else if (x > 0 && y < 0) {
+    document.write('se encuentra en el cuarto cuadrante');
+}
+
+// ejercicio 5
+
+let sueldo = Number(prompt("ingrese el sueldo"));
+let antiguedad = Number(prompt("antiguedad"));
+
+if (sueldo < 500 && antiguedad >= 10) {
+    let porcentaje = (20 / 100) * sueldo;
+    document.write("se le orotgo un aumento del 20%, el total ahora es: " + (porcentaje + sueldo));
+}else if (sueldo < 500 && antiguedad < 10) {
+    let porcentaje = (5 / 100) * sueldo;
+    document.write("se le otorgo un aumento del 5%, el total ahora es: " + (porcentaje + sueldo));
+}else {
+    document.write("su sueldo es de: " + sueldo);
+}
+
+
+
+
+
 
 
 
